5b36a1032687c874c95dde778ee17d826acec1d5
learnpaddi <thangadurai@learnpaddi.in>
2025-06-23 21:48:47 +0530

    30

--- sendmail.js
+++ sendmail.js
@@ -2,8 +2,7 @@ function sendMail() {
     var form = document.getElementById('careerForm');
     emailjs.sendForm('service_jzzspeo', 'template_4s4af01', form)
         .then(function(response) {
-            alert("Mail sent successfully!");
-            form.reset();
+            window.location.href = "thank-you.html"; // Redirect on success
         }, function(error) {
             alert("Failed to send mail: " + error.text);
         });
