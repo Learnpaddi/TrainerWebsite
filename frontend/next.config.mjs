03fd21a3e56a0411d97e7f369bbebba064efa0c1
Learnpaddi <thangadurai@learnpaddi.in>
2026-04-02 08:22:34 +0000

    Reset tracking

--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    esmExternals: 'loose',
-  },
-  transpilePackages: ['lucide-react'],
-};
-
-export default nextConfig;
